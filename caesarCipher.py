435e5da03847d58cb2c2351e33b57e6d753ba69f
VadimkaLYZ <105925217+VadimkaLYZ@users.noreply.github.com>
2022-11-30 17:20:21 +0200

    Шифр Цезаря

---
+++ caesarCipher.py
@@ -0,0 +1,45 @@
+#шифр Цезаря
+
+import pyperclip
+
+#Ця стрічка підлягає шифруванню/дешифруванню
+message = 'This is my secret message.'
+
+#Ключ шифрування
+key = 13
+
+#Встановвлення режиму роботи: шифрування або дешифрування
+
+mode = 'encrypt' # 'encrypt' or 'decrypt'
+
+#Всі символи які можуть бути  зашифровані
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+#Змінна яка зберігає зашифровану/дешифровану форму повідомлення
+translated = ''
+
+for symbol in message:
+    #Замітка: шиф/деш можна тільки символи, які включені в строку SYMBOLS
+    if symbol in SYMBOLS:
+        symbolIndex = SYMBOLS.find(symbol)
+
+        # Виконати шифрування/дешифрування
+        if mode == 'encrypt':
+            translatedIndex = symbolIndex + key
+        elif mode == 'decrypt':
+            translatedIndex = symbolIndex - key
+        # Опрацювати "завертання", якщо необхідно
+
+        if translatedIndex >= len(SYMBOLS):
+            translatedIndex = translatedIndex - len(SYMBOLS)
+        elif translatedIndex < 0:
+            translatedIndex = translatedIndex + len(SYMBOLS)
+
+        translated = translated + SYMBOLS[translatedIndex]
+    else:
+        #приєднання символу без шифрування
+        translated = translated + symbol
+
+#Виввід переброзаваной строки
+print(translated)
+pyperclip.copy(translated)
+
